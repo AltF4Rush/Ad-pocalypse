2cb433f164ace4d372b468d11dca26d0aec8cc15
darkkboy0013@gmail.com <darkkboy0013@gmail.com>
2025-03-07 12:47:30 +0000

    first commit

---
+++ build/_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/Unix/KeySymToKeyMapping.cpp.o.d
@@ -0,0 +1,9 @@
+_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/Unix/KeySymToKeyMapping.cpp.o: \
+ /home/kaido/Projects/Ad-pocalypse/build/_deps/sfml-src/src/SFML/Window/Unix/KeySymToKeyMapping.cpp \
+ /usr/include/stdc-predef.h \
+ /home/kaido/Projects/Ad-pocalypse/build/_deps/sfml-src/src/SFML/Window/Unix/KeySymToKeyMapping.hpp \
+ /home/kaido/Projects/Ad-pocalypse/build/_deps/sfml-src/include/SFML/Window/Keyboard.hpp \
+ /home/kaido/Projects/Ad-pocalypse/build/_deps/sfml-src/include/SFML/Window/Export.hpp \
+ /home/kaido/Projects/Ad-pocalypse/build/_deps/sfml-src/include/SFML/Config.hpp \
+ /usr/include/X11/X.h /usr/include/X11/keysym.h \
+ /usr/include/X11/keysymdef.h
